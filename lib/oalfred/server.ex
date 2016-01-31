6450f045c838f58d1c6bfd8012d138b4867579c5
pedrosnk <pedrosnk@gmail.com>
2016-01-31 14:48:22 -0200

    add user rest methods

--- lib/oalfred/server.ex
+++ lib/oalfred/server.ex
@@ -4,11 +4,43 @@ defmodule Oalfred.Server do
   plug Plug.Logger, log: :debug
   plug :match
   plug :dispatch
-
+  
   get "/auth" do
     conn
     |> put_resp_content_type("application/json")
     |> send_resp(200, Poison.encode!(%{auth: true}))
+  end
+
+  get "/users/:name" do
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(200, Poison.encode!(%{username: name}))
+  end
+
+  get "/users" do
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(200, Poison.encode!(%{users: []}))
+  end
+
+  post "/users" do
+    {:ok, body, _} = read_body(conn)
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(201, body)
+  end
+
+  put "/users" do
+    {:ok, body, _} = read_body(conn)
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(200, body)
+  end
+
+  delete "/users/:name" do
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(200, Poison.encode!(%{username: name}))
   end
 
 end
