71a2950680773c39347110cc91ac6fe2b98db838
Pedro Medeiros <pedrosnk@gmail.com>
2016-02-18 09:56:12 -0200

    Add AgentStore For users

---
+++ lib/oalfred/agent_store.ex
@@ -0,0 +1,29 @@
+defmodule Oalfred.AgentStore do
+
+  def start_link do
+    Agent.start_link(fn -> MapSet.new end, name: __MODULE__)
+  end
+
+  def add_user user do
+    Agent.update(__MODULE__, fn users ->
+      MapSet.put users, user
+    end)
+  end
+
+  def get_user_by_email email do
+    Agent.get(__MODULE__, fn users ->
+      Enum.find(users, fn user ->
+        user.email == email
+      end)
+    end)
+  end
+
+  def get_user_by_name name do
+    Agent.get(__MODULE__, fn users ->
+      Enum.find(users, fn user ->
+        user.email == name
+      end)
+    end)
+  end
+
+end
--- lib/oalfred/server.ex
+++ lib/oalfred/server.ex
@@ -4,7 +4,7 @@ defmodule Oalfred.Server do
   plug Plug.Logger, log: :debug
   plug :match
   plug :dispatch
-  
+
   get "/auth" do
     conn
     |> put_resp_content_type("application/json")
